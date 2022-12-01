f7155a83e0616858c16e995bea6e738d7bc09255
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-12-01 09:06:08 +0500

    use of custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,68 +1,14 @@
 import React,{ useState,useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home= () => {
-   const [blogs,setBlogs]= useState(null);
-   const [isPending, setIsPending] =useState(true);
-   const[error,setError]=  useState (null);
-     //[
-      //{
-        
-           /// id: 1,
-             //  title: " Tandori Chiken Tika ",
-              // body: "The natural red colouring in my recipe comes from the paprika and chilli powder I use. To enhance the taste as well as the colour add a pinch of crushed saffron strands to the marinade. I prefer chicken thighs which are a more succulent cut though chicken breast works equally well.This is also the basic marinade that is commonly used to cook the chicken before making Butter Chicken (aka Chicken Tikka Masala).",
-              // author: "noor",
-              //},
-             // {
-             //id: 2,
-               // title: "Mutton Biryni",
-                // body: "To make a delightful chicken biryani dish firstly soak saffron in water to prepare saffron water (One tsp saffron can be soaked in 1/4 cup water). Next, mix kewra drops in water and mix well to make kewra water. Set them aside for later usage. Now chop the onion and coriander leaves and keep them aside.',",
-             //author: "aqsa",
-              //},
-          // {
-              //id: 3,
-               // title: "Sheer Khurma ",
-                //body: "Sheer Khurma is a milky dessert cooked with vermicelli, dried dates and nuts. The word Sheer means milk in the Farsi language, and Khurma means dates = milk dates.Traditionally, dried dates are chopped and used in Sheer Khurma. Sheer Khurma is also known as Seviyan in some households (mine included!) and Shemai in Bangladesh, where it is often served alongside parathas. ",
-              //   author: "hania",
-            //},
-        //])
-          //console.log(blogs);
-           const [name,setName] = useState ('hania');
-          {/*const handleDelete =(id) => {
-           const newBlogs = blogs.filter(blog => blog.id!== id);
-           setBlogs(newBlogs);
-          }*/}
-
-          useEffect(() =>{
-            setTimeout(()=>{
-              fetch('http://localhost:8000/blogs')
-            .then(res =>{
-              //console.log(res);
-              if(!res.ok){
-                throw Error('could not fetch data that resource');
-              }
-              return res.json();
-            }).then(data => {
-              console.log(data);
-              setBlogs(data);
-              setIsPending(false);
-              setError(null);
-            })
-            .catch((err)=>{
-              setIsPending(false);
-              setError(err.message);
-              //console.log(err.message);
-            })
-            })
-            
-            //console.log('use effect ran');
-            //console.log(blogs);
-          },[1000]);
+   const{data:blogs,isPending,error} = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
           {error && <div>{error}</div>} 
         {isPending && <div>Loading...</div>}
-         {blogs && <BlogList blogs={blogs}title ="All Blogs!"/>}
+        {blogs && <BlogList blogs = {blogs} title ="All Blogs!"/>}
        {/*{blogs && <BlogList blogs={blogs}title ="All Blogs!" handleDelete= {handleDelete}/>}*/}
         {/*<BlogList blogs={blogs.filter((blog)=> blog.author==='hania')}title ="hania's blogs!"/>
        <button
