cda3d3735df1d15339b4eee33e0a159261876baf
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-12-18 13:24:40 +0500

    Delete blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 const BlogDetails = () => {
     const {id} = useParams();
-    const{data:blog,error,isPending} = useFetch('http://localhost:8000/blogs/' + id);
-  return (
+    const history = useHistory();
+    const{data:blog,error,isPending} = useFetch(`http://localhost:8000/blogs/${id}`);
+    console.log(blog);
+
+    //delete blog function
+    const handleDelete =() =>{
+      fetch(`http://localhost:8000/blogs/${blog.id}`,{
+      method:"DELETE",
+    }).then(()=>{
+     history.push("/");
+      console.log("blog deleted");
+    })
+    };
+      return (
   <div className="blog-details">
     {isPending && <div> Loading...</div>}
     {error && <div>{error}</div>}
@@ -13,6 +25,13 @@ const BlogDetails = () => {
         <h2>{blog.title}</h2>
         <p>written by {blog.author}</p>
         <div>{blog.body}</div>
+        <button
+            onClick={handleDelete}
+            className="btn btn-outline-danger d-grid gap-2 col-6 mx-auto"
+            // type="button"
+          >
+            Delete Blog
+          </button>
       </article>
     )}
      </div>
